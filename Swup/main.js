dcad9e33f97313189fc08c336a7b1bde1c0e4597
Stian have <stianhave@hotmail.com>
2020-05-20 21:58:49 +0200

    Added GSAP

--- Swup/main.js
+++ Swup/main.js
@@ -1,1 +1,57 @@
 const swup = new Swup();
+const tlOne = new TimelineLite({ paused: true });
+
+swup.on("animationInDone", function () {
+  console.log("animation done");
+  let art = document.querySelectorAll(".article");
+  animateArticles(art[0], art[1], art[2]);
+});
+
+swup.on("pageLoaded", function () {
+  console.log("loaded");
+  let art = document.querySelectorAll(".article");
+  animateArticles(art[0], art[1], art[2]);
+});
+
+swup.on("clickLink", function () {
+  tlOne.reversed() ? tlOne.play() : tlOne.reverse();
+});
+
+function animateArticles(a, b, c) {
+  //   var tlTwo = new TimelineLite({ paused: true });
+
+  tlOne
+    .to(a, 0.5, {
+      //animation
+      opacity: 1,
+    })
+    .to(
+      b,
+      0.5,
+      {
+        //animation
+        opacity: 1,
+      },
+      "-=.5"
+    )
+    .fromTo(
+      c,
+      0.5,
+      {
+        //from
+        opacity: 0,
+        y: -1000,
+      },
+      {
+        //to
+        opacity: 1,
+        y: 0,
+        onComplete: function () {
+          console.log("done");
+        },
+      },
+      "-=.5"
+    );
+
+  tlOne.play();
+}
